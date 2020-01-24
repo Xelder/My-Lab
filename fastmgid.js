3574c99826a40540649703cfe1c11af8bb6f56e0
Xelderz Grazers <57059899+Xelder@users.noreply.github.com>
2020-01-24 14:12:46 +0700

    Update fastmgid.js

--- fastmgid.js
+++ fastmgid.js
@@ -1,6 +1,6 @@
 (function () {
         var script = document.createElement('script');
-        script.src = "//jsc.mgid.com/b/i/namina_kiky.com.780378.js?t=" +  ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : '');
+        script.src = "//jsc.mgid.com/k/u/kumamori.com.772283.js?t=" +  ((d = new Date()) ? '' + d.getUTCFullYear() + d.getUTCMonth() + d.getUTCDate() + d.getUTCHours() : '');
         script.async = true;
         document.body.appendChild(script);
     })();
